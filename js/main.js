47ab5903c28ec4f2a6a2715af005066776c121d8
OviyaVishalini <oviyavishalini31@gmail.com>
2025-11-17 12:34:07 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,121 @@
+window.addEventListener("scroll", function(){
+ const header =this.document.querySelector("header")
+header.classList.toggle("sticky", this.window.scrollY > 0)
+});
+
+const portfolioModals = document.querySelectorAll(".portfolio-model");//show details
+const imgCards = document.querySelectorAll(".img-card");//detects click
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");//close model
+
+var portfolioModal = function (modalclick) {
+    portfolioModals[modalclick].classList.add("active");
+}//modalclick selects the index and active when it is clicked 
+
+imgCards.forEach((imgCard, i) => {//img card choose card and i - no.of the card
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});// img-card is project card and portfolio-model is models each card is linked to a card c1-m1, c2-m2
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
+
+//theme button
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+
+    localStorage.setItem("saved-theme", getCurrentTheme());
+    localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if(savedTheme){
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
+
+
+
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+    scrollTopBtn.classList.toggle("active" , this.window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;//when the scrollTopBtn clicks the entire html body goes to top.
+});
+
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+
+        }
+        else{
+             document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+
+        }
+    });
+});
+
+//responsive navigation menu toggle
+ const menuBtn = document.querySelector(".nav-menu-btn");
+  const CloseBtn = document.querySelector(".nav-close-btn");
+   const navigation = document.querySelector(".navigation");
+   const navItems = document.querySelectorAll(".nav-items");
+
+   menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+   });
+
+   CloseBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+   });
+
+   navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+        });
+   });
+
+   //scroll reveal animations
+   //Common reveal options to create animations
+   ScrollReveal({
+    //reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 100
+ });
+
+ //target elements and specify options to create reveal animations
+ ScrollReveal().reveal('.home .info h2, .section-title-01, section-title-02', {delay: 500, origin:'left'});
+  ScrollReveal().reveal('.home .info h3, .home .info p, .about-infor .btn', {delay: 600, origin:'right'});
+   ScrollReveal().reveal('.home .info .btn', {delay: 700, origin:'bottom'});
+   ScrollReveal().reveal('.contact-left li, .contact-left h2', {delay:500, origin:'left',interval:200});
+   ScrollReveal().reveal('.home-img, .about-img', {delay: 500, origin:'bottom'});
+   ScrollReveal().reveal('.about .description, .contact-right', {delay: 500, origin:'right'});
+      ScrollReveal().reveal('.skills-description, .scontact-left h2', {delay: 700, origin:'left'});
+      ScrollReveal().reveal('.education, .projects .img-card', {delay: 800, origin:'bottom', interval:200});
+       ScrollReveal().reveal('footer, .group', {delay: 500, origin:'top', interval:200});
